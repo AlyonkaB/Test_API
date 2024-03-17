a96499b9700f17865b8bab5394431eca594861f8
Alona <alyonkabagritskaya@gmail.com>
2024-03-17 12:30:23 +0200

    допрацюванДопрацювання Адмінки, кÐ

--- db/admin.py
+++ db/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 from db.models import Bus, Facility, Trip, Order, Ticket
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
 admin.site.register(Bus)
 admin.site.register(Facility)
 admin.site.register(Trip)
-admin.site.register(Order)
 admin.site.register(Ticket)
