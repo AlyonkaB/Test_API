a4dc9a40373c6f27b6ead7dab0fa0efda72b3689
Alona <alyonkabagritskaya@gmail.com>
2024-05-25 22:55:32 +0300

    fix flake8 in user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from user.views import CreateUserView, LoginUserClass, ManagerUserView
 from rest_framework.authtoken import views
@@ -10,7 +14,7 @@ urlpatterns = [
     # path('api-token-auth/', LoginUserClass.as_view(), name="get_token"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManagerUserView.as_view(), name="manage_user"),
 ]
 
